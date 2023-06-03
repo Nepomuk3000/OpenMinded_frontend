c356ddeab36a8385373e87d539920dde7bcb42a2
Michael Luitaud <angular@luitaud.fr>
2023-06-03 20:17:20 +0200

    Chargement dynamique des utilisateurs dans la gallerie

--- openMined_frontEnd/src/app/users-gallery/users-gallery.component.ts
+++ openMined_frontEnd/src/app/users-gallery/users-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 
@@ -10,20 +10,64 @@ import { UserService } from '../../services/user.service';
 export class UsersGalleryComponent implements OnInit {
 
   users: User[] = [];
+  lastLoadedUser:number = 0;
+  noMoreUsers=false;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     // TODO : Charger les utilisateurs au fur et à mesure du scroll et pas tous d'un coup
-    this.loadUsers();
+    
+    this.loadComponentsIfNeeded();
   }
 
-  loadUsers() {
-    this.userService.getUsers().subscribe((users: User[]) => {
+  loadUsers(count:number,skip:number) {
+    this.userService.getUsers(count,this.lastLoadedUser).subscribe((users: User[]) => {
+      if (users.length == 0)
+      {
+        this.noMoreUsers=true;
+      }
       users.forEach(user => {
         this.users.push(user);
-        console.log("add user " + user.username);
       });
     });
+    this.lastLoadedUser += count;
+  }
+  
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+    this.loadComponentsIfNeeded();
+  }
+
+  loadComponentsIfNeeded()
+  {    
+    const windowHeight = window.innerHeight;
+    const documentHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
+    if (documentHeight <= window.scrollY + 2 * windowHeight && this.noMoreUsers == false) {
+      // Charger les nouveaux composants
+      this.loadUsers(10,this.lastLoadedUser);
+
+      // Appeler la fonction de chargement à nouveau après un certain délai
+      setTimeout(() => {
+        this.loadComponentsIfNeeded();
+      }, 20); // ajustez le délai en fonction de vos besoins
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    this.loadComponentsIfNeeded();
+  }
+
+  handleRejectUser()
+  {
+    console.log("TODO supprimer l'utilisateur rejeté de la gallerie");
   }
 }
