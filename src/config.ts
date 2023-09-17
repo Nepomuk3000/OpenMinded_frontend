db52ca3d0349bb3628b146103463b9a8062aa225
Michael Luitaud <angular@luitaud.fr>
2023-09-17 18:17:23 +0200

    Utilisation de l'API sur openminded.luitaud.fr

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
 // TODO : faire foncionner la liaison en https
-export const serverUrl = 'http://82.165.184.102:3000'; // Remplacez par votre URL d'API réelle
+export const serverUrl = 'https://openminded.luitaud.fr'; // Remplacez par votre URL d'API réelle
